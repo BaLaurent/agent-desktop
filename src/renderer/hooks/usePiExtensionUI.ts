58d37d09b0a88b77d7dc5f87565a0b2d4f2495ea
Laurent Baaziz <d3adlol0@gmail.com>
2026-02-28 19:47:58 +0100

    feat(pi-ui): add hook to route IPC extension UI events to store

---
+++ src/renderer/hooks/usePiExtensionUI.ts
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import { usePiExtensionUIStore } from '../stores/piExtensionUIStore'
+import type { PiUIRequest, PiUIEvent } from '../../shared/piUITypes'
+
+export function usePiExtensionUI(): void {
+  const enqueueDialog = usePiExtensionUIStore((s) => s.enqueueDialog)
+  const addNotification = usePiExtensionUIStore((s) => s.addNotification)
+  const setStatusEntry = usePiExtensionUIStore((s) => s.setStatusEntry)
+  const clearStatusEntry = usePiExtensionUIStore((s) => s.clearStatusEntry)
+  const setWidget = usePiExtensionUIStore((s) => s.setWidget)
+  const clearWidget = usePiExtensionUIStore((s) => s.clearWidget)
+  const setWorkingMessage = usePiExtensionUIStore((s) => s.setWorkingMessage)
+  const setHeaderComponent = usePiExtensionUIStore((s) => s.setHeaderComponent)
+  const setFooterComponent = usePiExtensionUIStore((s) => s.setFooterComponent)
+  const setTitleOverride = usePiExtensionUIStore((s) => s.setTitleOverride)
+
+  useEffect(() => {
+    const unsubRequest = window.agent.pi.onUIRequest((request: PiUIRequest) => {
+      enqueueDialog(request)
+    })
+
+    const unsubEvent = window.agent.pi.onUIEvent((event: PiUIEvent) => {
+      switch (event.method) {
+        case 'notify':
+          addNotification(event.message, event.level || 'info')
+          break
+        case 'setStatus':
+          if (event.text != null) setStatusEntry(event.key, event.text)
+          else clearStatusEntry(event.key)
+          break
+        case 'setWidget':
+          if (event.content != null) setWidget(event.key, event.content, event.placement || 'belowEditor')
+          else clearWidget(event.key)
+          break
+        case 'setWorkingMessage':
+          setWorkingMessage(event.message)
+          break
+        case 'setTitle':
+          setTitleOverride(event.title)
+          break
+        case 'setHeader':
+          setHeaderComponent(event.component ?? null)
+          break
+        case 'setFooter':
+          setFooterComponent(event.component ?? null)
+          break
+      }
+    })
+
+    return () => {
+      unsubRequest()
+      unsubEvent()
+    }
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps -- subscribe once, unsubscribe on unmount
+}
